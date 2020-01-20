ae67e767fd97a290bee2bf334af56be9d84896fe
jameskomo <james.komoh@gmail.com>
2020-01-20 10:41:17 +0300

    User profile model creation and relationships mapping

--- kinetic_freelance/urls.py
+++ kinetic_freelance/urls.py
@@ -22,7 +22,6 @@ from allauth.account import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('freelance_app.urls')),
-    path('profile/', user_views.profile, name='profile'),
     path('accounts/', include('allauth.urls')),
     
 ]
